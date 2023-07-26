734f7f1f79c7847f542770e46033ea04565a364b
Rajesh Babu <sammydave.e@gmail.com>
2023-07-26 16:16:14 +0200

    Fully working client infinite list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { LoadMore } from "@/components/load-more";
 
 interface Beer {
   id: number;
   name: string;
   tagline: string;
+  image_url: string;
   // Add other properties you want to display
 }
 
@@ -14,7 +16,7 @@ const ProductsPage = () => {
   const [page, setPage] = useState(1);
 
   const fetchProducts = async (page: number) => {
-    const perPage = 20;
+    const perPage = 50;
     const apiUrl = `https://api.punkapi.com/v2/beers?page=${page}&per_page=${perPage}`;
     try {
       const response = await fetch(apiUrl);
@@ -25,8 +27,13 @@ const ProductsPage = () => {
     }
   };
 
+  const delay = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+
   const loadMoreProducts = async () => {
-    const nextPage = page + 1;
+    // Once the page 8 is reached repeat the process all over again.
+    await delay(2000);
+    const nextPage = (page % 7) + 1;
     const newProducts = await fetchProducts(nextPage);
     setProducts((prevProducts) => [...prevProducts, ...newProducts]);
     setPage(nextPage);
@@ -37,22 +44,24 @@ const ProductsPage = () => {
   }, []);
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-4">Punk API Products Page</h1>
+    <div className="container mx-auto px-4 py-8 min-h-screen">
+      <h1 className="text-3xl font-bold mb-4">Infinite Drinks</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map((product) => (
           <div key={product.id} className="bg-white rounded shadow p-4">
+            <div className="mb-4">
+              <img
+                src={product.image_url}
+                alt={product.name}
+                className="object-contain w-full h-48 rounded"
+              />
+            </div>
             <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
             <p>{product.tagline}</p>
           </div>
         ))}
       </div>
-      <button
-        onClick={loadMoreProducts}
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-      >
-        Load More
-      </button>
+      <LoadMore onLoadMore={loadMoreProducts} />
     </div>
   );
 };
